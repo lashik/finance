2ae774419ca9012a7e7721edfd05a98537e89bf7
Kashik Sredharan <kashik.sredharan@gmail.com>
2025-04-15 21:04:16 +0530

    main

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://erhugbiefmpxmyhcxxej.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyaHVnYmllZm1weG15aGN4eGVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxNzQ4MTEsImV4cCI6MjA1NDc1MDgxMX0.pzhAQ_P6Keogfgyyi3tnAWlK_NZF8umfRViyWWfTqZM';
 export const supabase = createClient(supabaseUrl, supabaseKey);
